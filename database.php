9a6abdd72ffab6bf45fc40ead8c24e3b76c2a319
mgtipp <82818412+mgtipp@users.noreply.github.com>
2022-02-28 22:07:58 -0500

    Add files via upload
    Created php file to connect to the database, myDateList.

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+    //sets up a database called myDateList
+    $dsn = 'mysql:host=localhost;dbname=myDateList';
+    $username = 'root';
+    $password = '';
+    $dbname = 'myDateList';
+
+    //catches PDOException if the database does not work
+    try {
+        $db = new PDO($dsn, $username, $password, $dbname);
+    } catch (PDOException $e) {
+        $error_message = $e -> getMessage();
+        echo '<p> Unable to connect to the database: ' .$error;
+        exit();
+    }
+?>
