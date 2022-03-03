baa7cffe190a1f117e9abd6b6b54dc0ed1fd1e74
mgtipp <mt94449@uga.edu>
2022-03-02 22:00:14 -0500

    logout
    created logout action for the user

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 ?>
 
 <section class="index-form">
+    <?php
+        if (isset($_SESSION["usersUID"])) {
+            echo "<p>Hello there " . $_SESSION["usersUID"] . "</p>";
+        }
+    ?>
+
     <h1>Welcome to MyDateList. This is the homepage.</h1>
 </section>
 
--- myProfile.php
+++ myProfile.php
@@ -4,6 +4,7 @@
 
 <section class="myProfile-form">
     <h1>This is the Profile page. It is only accessible if you're logged in.</h1>
+    <a href="includes/logout.inc.php">Logout</a>
 </section>
 
 <?php
