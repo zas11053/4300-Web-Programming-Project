71576cc1cc49091c17744c207e703251a8f557fb
BlakeLaczynski <100541424+BlakeLaczynski@users.noreply.github.com>
2022-03-24 17:56:24 -0400

    Update about.php

--- about.php
+++ about.php
@@ -3,9 +3,13 @@
 include_once 'header.php';
 ?>
 
-<h1> This is the about page </h1>
+<h1> About us! </h1>
     
-
+<p> Over at MyDateList we love seeing couples and friends alike come together to make long lasting experiences. Now a days it seems 
+    that it's harder to get young adults out of the house, with all the technology out why would you? I mean, it seems easier to do so
+    anyway right? Wrong! Using MyDateList gives you a plethora of options to pick from, with the ability to not only favorite, comment
+    and save others dates, but we also allow you the ability to filter by location, price, and the type of activity as well. This website
+    allows for even the worlds most indecisive people to quickly find something to do! </p>
 
     
     
