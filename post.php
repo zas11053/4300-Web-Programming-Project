f106aee029cd3b4d438c8ad789287c94d444ef0f
tina liu <tl28625@uga.edu>
2022-03-31 04:17:23 -0400

    can currently click the radio button and change the img in the carousel.js rn

--- post.php
+++ post.php
@@ -4,16 +4,19 @@ include_once 'header.php';
 
 
    <div class="postBox">
-      <div class="imgBox">
+      <div class="imgBox" id="imgBox">
          <div class="carousel__item carousel__item--selected">
 
-            <div class="imgDropZone">
+            <div class="imgDropZone" id="imgDropZone">
                <span class="drop_prompt"> Drop file here or click to upload</span>
                <!--<div class="drop-zone-thumb" data-label="myFile.txt"> </div>-created by js-->
-               <input type ="file" name="imgFile" class="drop-zone-input" multiple>
+               <input type ="file" name="imgFile" id="imgFile" class="drop-zone-input" multiple>
             </div> 
 
          </div>
+
+         
+         
 
          <!---BELOW WILL BE PROVIDED BY JS
          <div class="carousel__nav">
@@ -27,9 +30,12 @@ include_once 'header.php';
          <h1> in text </h1>
       </div>
    </div>
+<!--
+     <script src="./js/imagePost.js" ></script>
+-->
+     <script src="./js/carousel.js" defer></script>
 
-     <script src="./js/imagePost.js"></script>
-     <script src="./js/carousel.js"></script>
+
 <?php
 include_once 'footer.php';
 ?>
