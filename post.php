48ddd28d1145ad004a1d5fbc6367e4087bd02ec3
tina liu <tl28625@uga.edu>
2022-03-31 02:16:28 -0400

    made it where images are added in imgDropZOne div[which is child of carousel__item div

--- post.php
+++ post.php
@@ -3,20 +3,33 @@ include_once 'header.php';
 ?>
 
 
-     <div class="postBox">
-     <div class="imgBox">
-        <div class="imgDropZone">
-           <span class="drop_prompt"> Drop file here or click to upload</span>
-          <!--<div class="drop-zone-thumb" data-label="myFile.txt"> </div>-created by js-->
-          <input type ="file" name="imgFile" class="drop-zone-input" multiple>
-        </div> 
-    </div> 
-        <div class="textBox">
-        <h1> in text </h1>
-        </div>
-     </div>
+   <div class="postBox">
+      <div class="imgBox">
+         <div class="carousel__item carousel__item--selected">
+
+            <div class="imgDropZone">
+               <span class="drop_prompt"> Drop file here or click to upload</span>
+               <!--<div class="drop-zone-thumb" data-label="myFile.txt"> </div>-created by js-->
+               <input type ="file" name="imgFile" class="drop-zone-input" multiple>
+            </div> 
+
+         </div>
+
+         <!---BELOW WILL BE PROVIDED BY JS
+         <div class="carousel__nav">
+            <span class="carousel__button"></span>
+
+         </div>
+         --->
+      </div> 
+
+      <div class="textBox">
+         <h1> in text </h1>
+      </div>
+   </div>
 
      <script src="./js/imagePost.js"></script>
+     <script src="./js/carousel.js"></script>
 <?php
 include_once 'footer.php';
 ?>
