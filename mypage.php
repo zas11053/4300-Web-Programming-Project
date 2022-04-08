0d17033db4c34aa806c6a426d4cd63f3d435729b
tina liu <tl28625@uga.edu>
2022-04-08 13:44:09 -0400

    deleted-b/c myPage doesn't need this
    I've selectively deleted this so can retrieve it later on --- myPage will just be viewing- no image upload will occur in myPage

--- mypage.php
+++ mypage.php
@@ -21,21 +21,7 @@ include_once 'header.php';
     $img=$image_dir['pfp_img_dir'];
     echo "<img style='width:100px; height: 100px' src= '$img'>";
 ?>
-<div style="margin-bottom: 3px; color: red; font-weight:bold;">
-    <?php
-        if(isset($_GET["error"])){
-            if($_GET["error"] == "toobig"){
-                echo "<p> Your file is too big!</p>";
-            } else if ($_GET["error"] == "empty"){
-                echo "<p> There was an error uploading your file! FILE IS PROB EMPTY </p>";
-            } else if ($_GET["error"] =="wrongtype"){
-                echo "<p> You cannot upload files of this type!</p>";
-            } else if ($_GET["error"] == "uploadsuccess"){
-                echo "<p style='color:green;'> You have changed your picture :) </p>";
-            }
-        }
-    ?>
-</div>
+
 <!-- Code for upload button -->
 <form action ="uploadProfile.php" method ="POST" enctype="multipart/form-data">
     <input type ="file" name="file">
