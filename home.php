a2f0a3e78d7f08b98946e46987716999288a3d76
tina liu <tl28625@uga.edu>
2022-04-14 18:52:57 -0400

    made it scrollable left to right hehhe

--- home.php
+++ home.php
@@ -5,6 +5,31 @@
 
 <div class ="whole-home">
 
+<div class="slideshow-container">
+        <!-- big sliding picture code -->
+        <?php
+            $sql1 = "SELECT * FROM imgs INNER JOIN posts ON posts.postID = imgs.postID"; // select everything from the imgs table to display on homepage
+            $result1 = mysqli_query($conn, $sql1);
+            $resultsCheck = mysqli_num_rows($result1); // error handling to make sure you're selecting something
+
+            if ($resultsCheck > 0) {
+                $slideshowNum = 0;
+                while ($row = mysqli_fetch_assoc($result1)) {
+                    $imgLink = $row['img_dir'];
+                    $slideshowNum++;
+                    if ($slideshowNum < 4) {
+                        ?>
+                      
+                            <div class="mySlides fade">
+                                <div class="numbertext"><?php echo $slideshowNum ?>/ 3</div>
+                                <img src=<?php echo $imgLink;?> alt="<?php echo $imgLink;?>">
+                            </div>
+                        <?php
+                    }
+                }
+            }
+        ?>
+</div>
 
        
 
@@ -22,10 +47,10 @@
 
         <div class ="top-post-slider">
         
-    <section id="post-gallery" class="wrapper-post">
+    <section id="post-gallery-home" class="wrapper-post">
     <?php for ($i = 0; $i < 10; $i++){ ?>
                 
-            <div class="card" style="position:relative;">
+            <div class="card" style="position:relative; width:320px">
 
                 <!---Makes the entire post a link, so when you click, takes you to the page to view the post-->
                 <a href="#">
