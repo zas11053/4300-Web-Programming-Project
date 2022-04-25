c929d4c75ea095ff23be26db0cee55a54136799b
tina liu <tl28625@uga.edu>
2022-04-25 19:04:53 -0400

    Updated dabase connection with login and password
    username: mydatelist
    password: Date123456!!
    if you don't wanna create the database login account just uncomment it and make it to root andn empty string

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,10 @@
 <?php
 
 $serverName = "localhost"; // because using xxamp
-$dBUsername = 'root'; //default
-$dBPassword = "";
+//$dBUsername = 'root'; //default
+$dBUsername = 'mydatelist'; //default
+//$dBPassword = "";
+$dBPassword = "Date123456!!";
 $dBName = "mydatelist"; // depend on what we call our database in mysql
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
