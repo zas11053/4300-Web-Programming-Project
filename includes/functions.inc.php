809a29508471bed9d6006197ebdf775258b6b4f7
tina liu <tl28625@uga.edu>
2022-04-25 17:55:59 -0400

    aah changed back to login page after created acc

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -106,7 +106,7 @@ function createUser($conn,$name, $username, $password, $question1, $question2, $
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
    
-    header("location: ../home.php?createdAccount"); 
+    header("location: ../login.php?error=none");
     exit(); //stops the script
     
     
